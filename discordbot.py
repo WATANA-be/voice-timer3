4f0790087e10a5258dd9de0f93669b32c3ff39fe
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2020-09-08 12:10:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,18 +8,18 @@ token = os.environ['DISCORD_BOT_TOKEN']
 pretime_dict = {}
 
 @bot.event
-async def on_voice_state_update(before, after):
-  print("ボイスチャンネルで変化がありました")
+    async def on_voice_state_update(before, after):
+    print("ボイスチャンネルで変化がありました")
 
-  if((before.voice.self_mute is not after.voice.self_mute) or (before.voice.self_deaf is not after.voice.self_deaf)):
-    print("ボイスチャンネルでミュート設定の変更がありました")
-    return
+        if((before.voice.self_mute is not after.voice.self_mute) or (before.voice.self_deaf is not after.voice.self_deaf)):
+            print("ボイスチャンネルでミュート設定の変更がありました")
+            return
 
-  if(before.voice_channel is None):
-    pretime_dict[after.name] = datetime.datetime.now()
-  elif(after.voice_channel is None):
-    duration_time = pretime_dict[before.name] - datetime.datetime.now()
-    duration_time_adjust = int(duration_time.total_seconds()) * -1
+        if(before.voice_channel is None):
+            pretime_dict[after.name] = datetime.datetime.now()
+        elif(after.voice_channel is None):
+        duration_time = pretime_dict[before.name] - datetime.datetime.now()
+        duration_time_adjust = int(duration_time.total_seconds()) * -1
 
     reply_channel_name = "general"
     reply_channel = [channel for channel in before.server.channels if channel.name == reply_channel_name][0]
