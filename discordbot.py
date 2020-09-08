62ed3ef03cd65e500827a0b3a1246f54f6cf4a3d
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2020-09-08 21:03:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,29 @@
+import discord
 from discord.ext import commands
 import os
 import traceback
+client = discord.Client()
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_voice_state_update(before, after):
+    print("ボイスチャンネルで変化がありました")
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+if(before.voice_channel is None):#入室時
+    pretime_dict[after.name] = datetime.datetime.now()
+else(after.voice_channel is None):#退出時
+    duration_time = pretime_dict[before.name] - datetime.datetime.now()
+    duration_time_adjust = int(duration_time.total_seconds()) * -1
+    
+reply_channel_name = "雑談"
+reply_channel = [channel for channel in before.server.channels if channel.name == reply_channel_name][0]
 
+reply_text = after.name + "　が　"+ before.voice_channel.name + "　から抜けました。　通話時間：" + str(duration_time_adjust) +"秒"
 
+await client.send_message(reply_channel ,reply_text)
+
+    
 bot.run(token)
+client.run("token")
