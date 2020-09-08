cc35ce2420845e2f76bd282f8d5cd2b3f6d49fd4
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2020-09-08 23:00:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,22 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    
+const mainChannelId = [738726518292742247];
+
+client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
+ if(oldGuildMember.voiceChannelID === undefined && newGuildMember.voiceChannelID !== undefined){
+   if(client.channels.get(newGuildMember.voiceChannelID).members.size == 1){
+     if (newGuildMember.voiceChannelID == "725595164105768984") {
+       newGuildMember.voiceChannel.createInvite({"maxAge":"0"})
+         .then(invite => sendMsg(
+           mainChannelId, "<@" + newGuildMember.user.id +"> が通話を開始しました！\n" + invite.url
+         ));
+     }
+   }
+ }
+});
 
 
 bot.run(token)
