5c7911ab66ef364dbbc8cec81068296e191600ec
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2020-09-09 11:08:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,20 +6,16 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@client.event
+    @client.event
 async def on_voice_state_update(member, before, after): 
     if member.guild.id == '738726517735030906' and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
@@ -30,7 +26,4 @@ async def on_voice_state_update(member, before, after):
         elif after.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
             await alert_channel.send(msg)
-    
-
-
 bot.run(token)
