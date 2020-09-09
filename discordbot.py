53f81db688e6b8f7987f72db6d8ddcb4f51c99e1
Watanabe <64056412+WATANA-be@users.noreply.github.com>
2020-09-09 21:16:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,29 @@
 import discord
+from discord.ext import commands
+import os
+import traceback
+import datetime
+from datetime import timedelta
 client = discord.Client()
-
-# 接続できたときに実行される
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 @client.event
-async def on_ready():
-    # 初期化処理などが行えるよ
-    print("On ready")
-
-if __name__ == "__main__":
-    client.run("752707979177885808")
+async def on_voice_state_update(member, before, after):
+    if member.guild.id == 752537435468071003 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(752537435468071006)
+        if before.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
+bot.run(token)
